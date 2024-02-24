5a59aeb2b8614a78ebcfcf536e6bf222dd0f645e
kim gon <ak0150a01@gmail.com>
2024-02-24 21:38:12 +0900

    diary 상세보기

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -6,39 +6,12 @@ import MyButton from "./MyButton";
 import MyHeader from "./MyHeader";
 import EmotionItem from "./EmotionItem";
 
-const emotionList = [
-    {
-        emotion_id : 1,
-        emotion_img : process.env.PUBLIC_URL + `/assets/emotion1.png`,
-        emotion_descript:'완전 좋음'
-    },
-    {
-        emotion_id : 2,
-        emotion_img : process.env.PUBLIC_URL + `/assets/emotion2.png`,
-        emotion_descript:'좋음'
-    },
-    {
-        emotion_id : 3,
-        emotion_img : process.env.PUBLIC_URL + `/assets/emotion3.png`,
-        emotion_descript:'그럭저럭'
-    },
-    {
-        emotion_id : 4,
-        emotion_img : process.env.PUBLIC_URL + `/assets/emotion4.png`,
-        emotion_descript:'나쁨'
-    },
-    {
-        emotion_id : 5,
-        emotion_img : process.env.PUBLIC_URL + `/assets/emotion5.png`,
-        emotion_descript:'끔찍함'
-    },
-];
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
 
-const getStringDate = (date) => {
-    return date.toISOString().slice(0,10);
-};
 
-const DiaryEditor = (isEdit, originData) => {
+
+const DiaryEditor = ({isEdit, originData}) => {
     const contentRef = useRef();
     const [content, setContent] = useState("");
     const [emotion, setEmotion] = useState(3);
@@ -70,7 +43,7 @@ const DiaryEditor = (isEdit, originData) => {
 
     useEffect(() =>{
         if(isEdit){
-            setDate(getStringDate(new Date(parseInt(originData.Date))));
+            setDate(getStringDate(new Date(parseInt(originData.date))));
             setEmotion(originData.emotion);
             setContent(originData.content);
         }
